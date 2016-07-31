46f9c092a7aadd0c0a1fcca4a617ea26d209969b
Bob Schmitz <rgschmitz1@madisoncollege.edu>
2016-07-31 20:50:10 +0000

    Minor index fixup
    Signed-off-by: Bob Schmitz <rgschmitz1@madisoncollege.edu>

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require_once('header.php');
-if ($_SERVER['PHP_SELF'] != "/inventory/index.php")
-    header("Location: /inventory/index.php");
+if ($_SERVER['PHP_SELF'] != "inventory/index.php")
+    header("Location: inventory/index.php");
 require_once('footer.php');
