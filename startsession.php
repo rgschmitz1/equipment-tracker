ea9955e154432cb990fea1b611c688d1c72b9ef4
Bob Schmitz <bschmitz@xes-inc.com>
2016-10-13 13:48:22 -0500

    Create session directories recursively
    Signed-off-by: Bob Schmitz <bschmitz@xes-inc.com>

--- startsession.php
+++ startsession.php
@@ -5,7 +5,7 @@ function startAppSession() {
     if ((session_save_path() != SESSION_DIR) || !isset($_SESSION)) {
         // Check if session directory exists or create it here
         if (!is_dir(SESSION_DIR)) {
-            mkdir(SESSION_DIR, 0750);
+            mkdir(SESSION_DIR, 0750, ture);
         }
         session_save_path(SESSION_DIR);
         session_start();
