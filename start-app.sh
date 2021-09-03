c91a4a6d6c857a8cedf6f0b59d41991aad73d2ca
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-09-03 23:01:29 +0100

    Importing app setup data using docker-compose
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- start-app.sh
+++ start-app.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
 docker-compose down
-docker-compose up -d
-while true; do
-	sleep 3
-	sudo docker exec -i mariadb mysql -umfgtest -p'Qty[U0Fy;hff9g3&~~Xb8bbqD]cMGM>x' < site/xes.sql && break
-done
+docker-compose up
